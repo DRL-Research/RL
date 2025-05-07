5c5b81fa75185389acb30a6c7eb95ee676791cff
Ariel <arielharush96@gmail.com>
2025-05-07 21:18:09 +0300

    AFTER CR FOR CFG

--- utils/model/model_handler.py
+++ utils/model/model_handler.py
@@ -1,24 +1,5 @@
 import os
-from dataclasses import dataclass
 from stable_baselines3 import PPO, DQN, A2C
-from sympy.categories.baseclasses import Class
-
-
-class CarName:
-    CAR1 = "Car1"
-    CAR2 = "Car2"
-    CAR3 = "Car3"
-    CAR4 = "Car4"
-    CAR5 = "Car5"
-
-
-class Role:
-    CAR1 = CarName.CAR1
-    CAR2 = CarName.CAR2
-    CAR3 = CarName.CAR3
-    CAR4 = CarName.CAR4
-    CAR5 = CarName.CAR5
-
 
 
 class ModelType:
