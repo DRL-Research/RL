471858397b66118c80e71b5c77683fc302c6db87
GilLifshits <lifshitsgil@gmail.com>
2026-07-14 10:07:08 +0300

    Most stable version

--- run_parallel_experiment.py
+++ run_parallel_experiment.py
@@ -125,11 +125,11 @@ def main():
     algorithms = {
         # "MAPS": "experiment",
         # "VN-MA-DDPG": "vn_maddpg",
-        # "MA-GA-DDPG": "ma_ga_ddpg",
-        # "IPPO": "ippo",
-        # "COMA": "coma",
-        # "VDN": "vdn",
-        # "Social-Attention": "social_attention",
+        "MA-GA-DDPG": "ma_ga_ddpg",
+        "IPPO": "ippo",
+        "COMA": "coma",
+        "VDN": "vdn",
+        "Social-Attention": "social_attention",
         "IDM": "idm"
     }
 
